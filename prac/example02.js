b1b87955e2a69b3442c045fa889f1533286fe051
유관우 <skymill2000@gmail.com>
2023-03-13 15:28:24 +0900

    work01(화살표 함수 구현) : 화살표 함수를 통한 곱셈 나눗셈 구현

--- prac/example02.js
+++ prac/example02.js
@@ -7,7 +7,16 @@ const minus = (p1, p2) => {
 };
 
 //arrow function 으로 곱하기 나누기 구현하기
+const multi = (p1, p2) => {
+  console.log(p1 * p2);
+};
+
+const div = (p1, p2) => {
+  console.log(p1 / p2);
+};
 
 sum(4, 2);
 minus(5, 1);
 minus(3, 2);
+multi(3, 10);
+div(100, 5);
