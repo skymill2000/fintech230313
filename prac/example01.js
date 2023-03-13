b1b543d5618593879fb47b77696e82005ba3a1c8
유관우 <skymill2000@gmail.com>
2023-03-13 15:08:25 +0900

    lecture(변수선언)  - let, var, const 를 통한 변수 선언과 출력

---
+++ prac/example01.js
@@ -0,0 +1,13 @@
+var userName = "유관우";
+console.log(userName);
+
+let userAge = 35;
+console.log(userAge);
+
+const userGender = "Male";
+console.log(userGender);
+
+let userAgeK = 40;
+var userName = "중복";
+
+console.log(userName);
