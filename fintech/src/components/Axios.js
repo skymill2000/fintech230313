70175c5cad3d4a7138e730efb81c3c440460b6c4
유관우 <skymill2000@gmail.com>
2023-03-14 16:49:37 +0900

    LECT(REACT_AXIOS): package.json 파일 내용 추가  CORS 회피

--- fintech/src/components/Axios.js
+++ fintech/src/components/Axios.js
@@ -1,14 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 
 const Axios = () => {
+  const [news, setNews] = useState();
   const handleClick = () => {
-    axios.get("https://naver.com").then((response) => {
+    let url =
+      "/v2/everything?q=tesla&from=2023-02-14&sortBy=publishedAt&apiKey=78bc6ddd8cdb48ceac76f5f9b9dfc4c5";
+    axios.get(url).then((response) => {
       console.log(response.data);
+      setNews(response.data);
     });
   };
   return (
     <div>
+      {news}
       <button onClick={handleClick}>통신 발생</button>
     </div>
   );
