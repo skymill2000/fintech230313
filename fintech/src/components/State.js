9a4191b12523e1759776f66291b0fcb4e39bb633
유관우 <skymill2000@gmail.com>
2023-03-14 14:28:04 +0900

    LECT(이벤트 - 스테이트): 이벤트와 스테이트 실습

--- fintech/src/components/State.js
+++ fintech/src/components/State.js
@@ -1,8 +1,21 @@
+import { useState } from "react";
+
 const State = () => {
+  let [inputText, setInputText] = useState();
+  const handleChange = ({ target }) => {
+    const { value } = target;
+    setInputText(value);
+  };
+
+  const handleClick = () => {
+    console.log("클릭!");
+  };
+
   return (
     <div>
-      <input></input>
-      <button>출력</button>
+      <p>{inputText}</p>
+      <input onChange={handleChange}></input>
+      <button onClick={handleClick}>출력</button>
     </div>
   );
 };
