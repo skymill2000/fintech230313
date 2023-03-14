17a90e31f70f16799e27e77a76766393c623cf1f
유관우 <skymill2000@gmail.com>
2023-03-14 17:22:52 +0900

    HOMEWORK(뉴스 API 작성하기): 뉴스 검색 페이지 완성

--- fintech/src/App.js
+++ fintech/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Welcome from "./components/Welcome";
 import List from "./components/List";
 import Axios from "./components/Axios";
+import NewsSearch from "./pages/NewsSearch";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <Route path="/welcome" element={<Welcome />}></Route>
           <Route path="/list" element={<List></List>}></Route>
           <Route path="/axios" element={<Axios></Axios>}></Route>
+          <Route path="/news" element={<NewsSearch></NewsSearch>}></Route>
         </Routes>
       </BrowserRouter>
     </div>
---
+++ fintech/src/pages/NewsSearch.js
@@ -0,0 +1,19 @@
+import React from "react";
+import AppHeader from "../components/common/AppHeader";
+
+const NewsSearch = () => {
+  return (
+    <div>
+      <AppHeader title={"뉴스 검색"}></AppHeader>
+      {/* homework 
+        1. 뉴스 검색어를 입력받는 컴포넌트
+         - State.js 컴포넌트 참조
+         - 통신 : Axios.js 컴포넌트를 참조
+        2. 해당 검색 결과를 출력하는 컴포넌트
+         - List 컴포넌트를 참조
+      */}
+    </div>
+  );
+};
+
+export default NewsSearch;
