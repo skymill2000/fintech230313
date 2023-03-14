036f5b1485d54a3ccbe0eb26c1cad07c141289bd
유관우 <skymill2000@gmail.com>
2023-03-14 17:09:06 +0900

    LECT(REACT_AXIOS): 뉴스 검색어를 통해 검색 기능 완성

--- fintech/src/components/Axios.js
+++ fintech/src/components/Axios.js
@@ -2,18 +2,24 @@ import React, { useState } from "react";
 import axios from "axios";
 
 const Axios = () => {
+  const [searchInput, setSearchInput] = useState("");
   const [news, setNews] = useState();
   const handleClick = () => {
-    let url =
-      "/v2/everything?q=tesla&from=2023-02-14&sortBy=publishedAt&apiKey=78bc6ddd8cdb48ceac76f5f9b9dfc4c5";
+    let url = `/v2/everything?q=${searchInput}&from=2023-02-14&sortBy=publishedAt&apiKey=78bc6ddd8cdb48ceac76f5f9b9dfc4c5`;
     axios.get(url).then((response) => {
       console.log(response.data);
       setNews(response.data);
     });
   };
+
+  const handleChange = ({ target }) => {
+    const { value } = target;
+    setSearchInput(value);
+  };
+
   return (
     <div>
-      {news}
+      <input onChange={handleChange}></input>
       <button onClick={handleClick}>통신 발생</button>
     </div>
   );
