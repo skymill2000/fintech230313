2f7c895da74390bf4c78d39d9a4f7108b926578f
유관우 <skymill2000@gmail.com>
2023-03-14 13:43:30 +0900

    LECT(REACT_컴포넌트와 프롭스): 컴포넌트 작성 이후 프롭스를 통해 데이터 전달

--- fintech/src/App.js
+++ fintech/src/App.js
@@ -1,14 +1,18 @@
 import logo from "./logo.svg";
 import "./App.css";
 
-const Welcome = () => {
-  return <p>안녕하세요</p>;
+const Welcome = (props) => {
+  console.log(props);
+  return <p>안녕하세요 {props.userName} 님 반갑습니다. </p>;
 };
 
 function App() {
   return (
     <div className="App">
-      <Welcome></Welcome>
+      <Welcome userName="홍길동"></Welcome>
+      <Welcome userName="고길동"></Welcome>
+      <Welcome userName="둘리"></Welcome>
+      <Welcome userName="도우너"></Welcome>
     </div>
   );
 }
