4a86c16d9d16d9df07996019d72037b5b12ca920
유관우 <skymill2000@gmail.com>
2023-03-14 15:24:16 +0900

    LECT(REACT_router): 라우터 설치
     npm install react-router-dom
    http://localhost:3000/welcome 에서 확인 필요

--- fintech/src/App.js
+++ fintech/src/App.js
@@ -1,17 +1,15 @@
 import "./App.css";
-import List from "./components/List";
-import State from "./components/State";
-import Styled from "./components/Styled";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Welcome from "./components/Welcome";
 
 function App() {
   return (
     <div className="App">
-      <Welcome userName="홍길동"></Welcome>
-      <Welcome userName="고길동"></Welcome>
-      <State></State>
-      <List></List>
-      <Styled></Styled>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/welcome" element={<Welcome />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
