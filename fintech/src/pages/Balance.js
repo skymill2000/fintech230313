597b11e0facf1f92366977f85106d28d7255289a
유관우 <skymill2000@gmail.com>
2023-03-16 16:32:56 +0900

    LECT(FINTECH): 거래내역 조회 페이지 완성

--- fintech/src/pages/Balance.js
+++ fintech/src/pages/Balance.js
@@ -3,13 +3,16 @@ import React, { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import AppHeader from "../components/common/AppHeader";
 import axios from "axios";
+import TransactionList from "../components/balance/TransactionList";
 const Balance = () => {
   const [balance, setBalance] = useState(0);
+  const [transactionList, setTransactionList] = useState([]);
   const query = useLocation().search;
   const fintechUseNo = queryString.parse(query).fintechUseNo;
 
   useEffect(() => {
     getBalance();
+    getTransactionList();
   }, []);
 
   const genTransId = () => {
@@ -45,7 +48,37 @@ const Balance = () => {
     axios(option).then(({ data }) => {
       console.log(data);
       setBalance(data.balance_amt);
-      //   setBalance(data.res_list);
+    });
+  };
+
+  const getTransactionList = () => {
+    const accessToken = localStorage.getItem("accessToken");
+
+    //여기서 부터 작성 바랍니다.
+    const sendData = {
+      bank_tran_id: genTransId(),
+      fintech_use_num: fintechUseNo,
+      inquiry_type: "A",
+      inquiry_base: "D",
+      from_date: "20230301",
+      to_date: "20230316",
+      sort_order: "D",
+      tran_dtime: "20230316161200",
+    };
+
+    const option = {
+      method: "GET",
+      url: "v2.0/account/transaction_list/fin_num",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+        Authorization: `Bearer ${accessToken}`,
+      },
+      params: sendData,
+    };
+
+    axios(option).then(({ data }) => {
+      console.log(data);
+      setTransactionList(data.res_list);
     });
   };
 
@@ -53,6 +86,7 @@ const Balance = () => {
     <div>
       <AppHeader title={"잔액조회"}></AppHeader>
       귀하의 잔액은 :{balance} 입니다.
+      <TransactionList transactionList={transactionList}></TransactionList>
     </div>
   );
 };
