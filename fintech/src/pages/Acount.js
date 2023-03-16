a83de47f68c9a04d626aeda9ed8a8e1621950846
유관우 <skymill2000@gmail.com>
2023-03-16 15:10:35 +0900

    LECT(Fintech): 계좌 목록 표시 개발(2/2)

--- fintech/src/pages/Acount.js
+++ fintech/src/pages/Acount.js
@@ -1,10 +1,17 @@
 import axios from "axios";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import AppHeader from "../components/common/AppHeader";
+import MainAccountCard from "../components/main/MainAccountCard";
 
 const Acount = () => {
   const accessToken = localStorage.getItem("accessToken");
   const userSeqNo = localStorage.getItem("userSeqNo");
+
+  useEffect(() => {
+    getAccountList();
+  }, []);
+
+  const [accountList, setAccountList] = useState([]);
   const getAccountList = () => {
     const sendData = {
       user_seq_no: userSeqNo,
@@ -20,13 +27,21 @@ const Acount = () => {
     };
     axios(option).then(({ data }) => {
       console.log(data);
+      setAccountList(data.res_list);
     });
   };
-  getAccountList();
 
   return (
     <div>
       <AppHeader title={"계좌 목록"}></AppHeader>
+      {accountList.map((data) => {
+        return (
+          <MainAccountCard
+            bankName={data.bank_name}
+            fintechUseNo={data.fintech_use_num}
+          ></MainAccountCard>
+        );
+      })}
     </div>
   );
 };
