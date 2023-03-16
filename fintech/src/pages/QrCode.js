1e1c711764d8b4ab7f1e80c4fc266cb8ad58acfb
유관우 <skymill2000@gmail.com>
2023-03-16 17:04:11 +0900

    LECT(Fintech): 큐알 코드 작성 완료

--- fintech/src/pages/QrCode.js
+++ fintech/src/pages/QrCode.js
@@ -1,12 +1,27 @@
 import React from "react";
 import AppHeader from "../components/common/AppHeader";
 import { QRCodeSVG } from "qrcode.react";
+import styled from "styled-components";
+import { useLocation } from "react-router-dom";
+import queryString from "query-string";
+
+const QRBlock = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 1rem;
+`;
 
 const QrCode = () => {
+  const query = useLocation().search;
+  const fintechUseNo = queryString.parse(query).fintechUseNo;
   return (
     <div>
       <AppHeader title={"QR CODE"}></AppHeader>
-      <QRCodeSVG value="https://reactjs.org/" />
+      <QRBlock>
+        <QRCodeSVG value={fintechUseNo} />
+        <p>핀테크번호 : {fintechUseNo}</p>
+      </QRBlock>
     </div>
   );
 };
