fb5d8d2493ae6edaedd2a65ece563016586e0476
유관우 <skymill2000@gmail.com>
2023-03-17 14:33:05 +0900

    LECT(Fintech): 출금 , 입금 이체를 위한 기본 준비  - npm install react-slick --force  - npm install react-modal --force  - npm install slick-carousel --force

--- fintech/src/pages/QrReaderPage.js
+++ fintech/src/pages/QrReaderPage.js
@@ -1,24 +1,57 @@
 import React, { useState } from "react";
+import AppHeader from "../components/common/AppHeader";
 import { QrReader } from "react-qr-reader";
+import ModalWithdraw from "../components/withdraw/ModalWithdraw";
+import Modal from "react-modal";
 
 const QrReaderPage = () => {
-  const [data, setData] = useState("No result");
+  const [data, setData] = useState("120230044088951024630415"); //입급 대상 핀테크 번호
+  const [openModal, setOpenModal] = useState(true); //no camera : true
+  const CustomStyles = {
+    overlay: {
+      backgroundColor: "rgba(0, 0, 0, 0.7)",
+      zIndex: "9",
+    },
+    content: {
+      width: "95%",
+      border: `0 solid black`,
+      top: "50%",
+      left: "50%",
+      right: "auto",
+      bottom: "auto",
+      marginRight: "-50%",
+      transform: "translate(-50%, -50%)",
+      zIndex: "99999",
+    },
+  };
+
+  const closeModal = () => {
+    setOpenModal(false);
+  };
 
   return (
     <>
+      <AppHeader title={"QR 읽기"}></AppHeader>
       <QrReader
         onResult={(result, error) => {
           if (!!result) {
             setData(result?.text);
+            setOpenModal(true);
           }
-
           if (!!error) {
-            console.info(error);
           }
         }}
         style={{ width: "100%" }}
       />
       <p>{data}</p>
+
+      <Modal
+        isOpen={openModal}
+        style={CustomStyles}
+        onRequestClose={closeModal}
+      >
+        <ModalWithdraw tofintechno={data}></ModalWithdraw>
+      </Modal>
     </>
   );
 };
